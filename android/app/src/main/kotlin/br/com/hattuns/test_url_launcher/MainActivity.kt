dd093d86ecf78cd31914c1921f733de9de627433
Railton98 <railtonleal98@gmail.com>
2020-05-28 01:50:38 -0300

    tests with a package flutter_native_splash

--- android/app/src/main/kotlin/br/com/hattuns/test_url_launcher/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/hattuns/test_url_launcher/MainActivity.kt
@@ -2,5 +2,8 @@ package br.com.hattuns.test_url_launcher
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
